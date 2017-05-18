0b92ea1768647c3784552d2f6a8565a44adef0ec
brent-corona <brent@coronalabs.com>
2017-05-18 13:47:07 -1000

    Update config.lua

--- lua/config.lua
+++ lua/config.lua
@@ -1,11 +1,23 @@
--- config.lua
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
 
 application =
 {
 	content =
 	{
 		width = 320,
-		height = 480,
-		scale = "letterbox", -- zoom to fill screen, possibly cropping edges
+		height = 480, 
+		scale = "letterbox",
+		fps = 60,
+		
+		--[[
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
+		--]]
 	},
 }
